9975f42a8b6e07147fcaa4daace6bde773dfe387
Linus <linus.f@protonmail.ch>
2024-09-17 21:26:32 +0200

    Fixed small stuff

--- devenv.nix
+++ devenv.nix
@@ -1,12 +1,12 @@
 { pkgs, lib, config, inputs, ... }:
 let
   name = "axumBase";
-  
 in
 {
   env = {
     OPENSSL_DIR = "${config.env.DEVENV_PROFILE}";
   };
+
   packages = [
     pkgs.openssl.dev
     pkgs.cargo-watch
